8704c981615c4375944a3e4cabb6cce8af96529e
rindra <rindra.randriambololona@gmail.com>
2022-03-03 11:02:39 +0100

    refactor chance

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,14 +1,10 @@
+import java.util.Arrays;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
+    public static int chance(int... numbers)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return Arrays.stream(numbers).sum();
     }
 
     public static int yatzy(int... dice)
